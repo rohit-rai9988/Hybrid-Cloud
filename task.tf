d8a3ef8dc0e190f363ce91a26abb13f62890c390
rohit-rai9988 <66864183+rohit-rai9988@users.noreply.github.com>
2020-06-15 11:52:59 +0530

    Update task.tf

--- task.tf
+++ task.tf
@@ -3,6 +3,74 @@ provider "aws" {
   profile    = "cloud"
 }
 
+resource "aws_s3_bucket" "rohittttttt" {
+  bucket = "rohittttttt" 
+  acl    = "public-read"
+  tags = {
+    Name        = "rohittttttt" 
+  }
+  versioning {
+	enabled =true
+  }
+}
+
+resource "aws_s3_bucket_object" "s3object" {
+  bucket = "${aws_s3_bucket.rohittttttt.id}"
+  key    = "terraform.png"
+  source = "C:/Users/rohit.rai/Downloads/terraform.png"
+}
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "This is origin access identity"
+}
+
+resource "aws_cloudfront_distribution" "imagecf" {
+    origin {
+        domain_name = "rohittttttt.s3.amazonaws.com"
+        origin_id = "S3-rohittttttt"
+
+
+        s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+       
+    enabled = true
+      is_ipv6_enabled     = true
+
+    default_cache_behavior {
+        allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = "S3-rohittttttt"
+
+
+        # Forward all query strings, cookies and headers
+        forwarded_values {
+            query_string = false
+        
+            cookies {
+               forward = "none"
+            }
+        }
+        viewer_protocol_policy = "allow-all"
+        min_ttl = 0
+        default_ttl = 10
+        max_ttl = 30
+    }
+    # Restricts who is able to access this content
+    restrictions {
+        geo_restriction {
+            # type of restriction, blacklist, whitelist or none
+            restriction_type = "none"
+        }
+    }
+
+
+    # SSL certificate for the service.
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+}
+
 
 resource "aws_security_group" "mysg" {
   name        = "mysg"
