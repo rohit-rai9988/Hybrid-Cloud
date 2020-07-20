e750c3fbd80d93927d2e58e7368f6b7abcfb613c
rohit-rai9988 <66864183+rohit-rai9988@users.noreply.github.com>
2020-07-20 14:27:29 +0530

    Update task3.tf

--- task3.tf
+++ task3.tf
@@ -146,4 +146,43 @@ tags = {
 
 }
 
+resource "aws_security_group" "mysc2" {
+ name = "mysc2-db"
+ description = "Allow inbound traffic"
+ vpc_id = aws_vpc.task3.id
+ingress {
+ from_port = 3306
+ to_port = 3306
+ protocol = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+ }
+ingress {
+ from_port = 22
+ to_port = 22
+ protocol = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+ }
+egress {
+ from_port = 0
+ to_port = 0
+ protocol = "-1"
+ cidr_blocks = ["0.0.0.0/0"]
+ }
+tags = {
+ Name = "allow_db"
+ }
+}
 
+resource "aws_instance" "db" {
+  ami = "ami-08f4fd653ae0c1e3d"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.mysc2.id]
+  subnet_id = aws_subnet.Private.id
+  key_name = "cloud"
+tags = {
+    Name = "mysql"
+  }
+}
+output "instance_ip_addr" {
+  value = aws_instance.db.private_ip
+}
